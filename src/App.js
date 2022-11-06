066f230e57adfc53da184a82264c583b172ee2bf
Konrad <konradbieniek@gmail.com>
2022-11-06 23:07:32 +0100

    set up Cart Contex

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { Fragment } from "react"
+import { useState } from "react"
 import Header from "./components/Layout/Header.js"
 import Meals from "./components/Meals/Meals.js"
+import Cart from "./components/Cart/Cart"
+import CartProvider from "./store/CartProvider.js"
 
 function App() {
+	const [cartIsShown, setCartIsShown] = useState(false)
+
+	const showCartHandler = () => {
+		setCartIsShown(true)
+	}
+
+	const hideCartHandler = () => {
+		setCartIsShown(false)
+	}
+
 	return (
-		<Fragment>
-			<Header />
+		<CartProvider>
+			{cartIsShown && <Cart onClose={hideCartHandler} />}
+			<Header onShowCart={showCartHandler} />
 			<main>
 				<Meals />
 			</main>
-		</Fragment>
+		</CartProvider>
 	)
 }
 
